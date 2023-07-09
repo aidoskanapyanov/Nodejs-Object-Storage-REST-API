54a8c2f911b2a50ff623f1f3b7c5e79279d010f1
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-07-09 16:46:06 +0600

    Add validation middleware for signup

---
+++ src/middlewares/validation.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+export const SignupSchema = z.object({
+  body: z.object({
+    id: z.string(),
+    // password should be at least 6 characters
+    password: z.string().min(6),
+  }),
+});
+
+export const validate = (schema) => (req, res, next) => {
+  try {
+    schema.parse({
+      body: req.body,
+      query: req.query,
+      params: req.params,
+    });
+
+    next();
+  } catch (err) {
+    return res.status(400).send(err.errors);
+  }
+};
