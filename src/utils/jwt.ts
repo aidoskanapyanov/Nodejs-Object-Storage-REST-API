30a4476cf05984ef75af680df5188433b58ac409
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-07-09 17:12:17 +0600

    Add utils for generating tokens

---
+++ src/utils/jwt.ts
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+export function createJwtTokens(user) {
+  const accessToken = jwt.sign(
+    { userId: user.id },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+
+  const refreshToken = jwt.sign(
+    { userId: user.id },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+  return { refreshToken, accessToken };
+}
