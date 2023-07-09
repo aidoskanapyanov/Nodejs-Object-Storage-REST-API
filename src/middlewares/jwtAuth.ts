f92c9f8864b010eac992b7e7fd0fb5a9b2381fbc
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-07-09 18:35:23 +0600

    Don't verify access token if blacklisted

--- src/middlewares/jwtAuth.ts
+++ src/middlewares/jwtAuth.ts
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import prisma from "../db";
 
 export default function authenticateJWT(req, res, next) {
   const authHeader = req.headers.authorization;
@@ -7,15 +8,27 @@ export default function authenticateJWT(req, res, next) {
     return res.status(401).json({ error: "No token provided" });
   }
 
-  const token = authHeader.split(" ")[1];
+  const accessToken = authHeader.split(" ")[1];
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ error: "Invalid token" });
+  jwt.verify(
+    accessToken,
+    process.env.ACCESS_TOKEN_SECRET,
+    async (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ error: "Invalid token" });
+      }
+
+      const isTokenInvalidated = await prisma.invalidatedToken.findUnique({
+        where: { accessToken },
+      });
+
+      if (isTokenInvalidated != null) {
+        return res.status(401).json({ error: "Invalid token" });
+      }
+
+      // Store the decoded token or user information in the request object for later use
+      req.user = decoded;
+      next();
     }
-
-    // Store the decoded token or user information in the request object for later use
-    req.user = decoded;
-    next();
-  });
+  );
 }
