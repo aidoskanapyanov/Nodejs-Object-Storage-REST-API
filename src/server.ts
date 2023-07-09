eea1219bf30515f1a21727699b109fbedcc06e6d
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-07-09 16:11:11 +0600

    Configure cors and logs

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
+import cors from "cors";
 import express from "express";
+import morgan from "morgan";
 import authenticateJWT from "./middlewares/jwtAuth";
 
 const app = express();
+
+app.use(cors()); // allow all origins
+app.use(morgan("dev")); // log status codes
 
 app.get("/", authenticateJWT, (req, res) => {
   res.json({
     message: "Hello World!",
   });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.json({ message: `had an error: ${err.message}` });
 });
 
 export default app;
