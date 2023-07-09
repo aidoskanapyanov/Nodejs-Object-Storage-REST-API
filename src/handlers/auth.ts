6487181c1f7eb13f012723faa205cf37bebc805a
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-07-09 17:40:16 +0600

    Add signin, refresh logic

--- src/handlers/auth.ts
+++ src/handlers/auth.ts
@@ -1,7 +1,74 @@
 import { Prisma } from "@prisma/client";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import prisma from "../db";
 import { createJwtTokens } from "../utils/jwt";
+
+export const signin = async (req, res) => {
+  const { id, password } = req.body;
+  try {
+    const user = await prisma.user.findUnique({
+      where: id.includes("@") ? { email: id } : { phoneNumber: id },
+    });
+    if (!user) {
+      res.status(400).json({ error: "Invalid username or password" });
+      return;
+    }
+    const passwordMatch = bcrypt.compareSync(password, user.passwordHash);
+    if (!passwordMatch) {
+      res.status(400).json({ error: "Invalid username or password" });
+      return;
+    }
+
+    const { accessToken, refreshToken } = createJwtTokens(user);
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+    });
+    res.json({ accessToken });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const refreshAccessToken = async (req, res) => {
+  const { refreshToken } = req.cookies;
+
+  // if no refresh token, send 401 unauthorized
+  if (!refreshToken) {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
+  try {
+    const { userId } = jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    // if user doesn't exist, send 401 unauthorized
+    if (!user) {
+      res.status(401).json({ error: "Unauthorized" });
+      return;
+    }
+
+    const { accessToken } = createJwtTokens(user);
+
+    res.json({ accessToken });
+  } catch (e) {
+    // if refresh token is expired, send 401 unauthorized
+    if (e instanceof jwt.TokenExpiredError) {
+      res.status(401).json({ error: "Refresh token expired. Sign in again" });
+      return;
+    }
+    res.status(401).json({ error: "Unauthorized" });
+  }
+};
 
 export const signup = async (req, res) => {
   const { id, password } = req.body;
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
 import morgan from "morgan";
-import { signup } from "./handlers/auth";
+import { refreshAccessToken, signin, signup } from "./handlers/auth";
 import authenticateJWT from "./middlewares/jwtAuth";
-import { SignupSchema, validate } from "./middlewares/validation";
+import { AuthSchema, validate } from "./middlewares/validation";
 
 const app = express();
 
 app.use(cors()); // allow all origins
 app.use(morgan("dev")); // log status codes
 app.use(express.json()); // parse json bodies (content-type: application/json)
+app.use(cookieParser());
 
 app.get("/", authenticateJWT, (req, res) => {
   res.json({
@@ -17,7 +19,9 @@ app.get("/", authenticateJWT, (req, res) => {
   });
 });
 
-app.post("/signup", validate(SignupSchema), signup);
+app.post("/signup", validate(AuthSchema), signup);
+app.post("/signin", validate(AuthSchema), signin);
+app.post("/signin/new_token", refreshAccessToken);
 
 app.use((err, req, res, next) => {
   console.log(err);
